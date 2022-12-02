ae48b9d2bf8b600a2c6e5a06794ebadbb30a43c7
zmacconnell <zachary.macconnell@gmail.com>
2022-12-02 11:01:20 -0700

    Added week 5 files

--- week04/scripts/task4.js
+++ week04/scripts/task4.js
@@ -3,7 +3,34 @@
 /* DATA */
 
 // Step 1: Declare a new variable to hold information about yourself
-
+let personalInfo = {
+    name: "Zach MacConnell",
+    photo: "images/main.png",
+    favoriteFoods: [
+        "pizza",
+        "hamburgers",
+        "sandwiches"
+    ],
+    hobbies: [
+        "reading",
+        "working on cars",
+        "playing games"
+    ],
+    placesLived: [
+        {
+            place: "Fairfield, CA",
+            length: "19 years"
+        },
+        {
+            place: "San Jose, CA",
+            length: "2 years"
+        },
+        {
+            place: "Rexburg, ID",
+            length: "2 years"
+        }
+    ]
+}
 // Step 2: Inside of the object, add a property named name with a value of your name as a string
 
 // Step 3: Add another property named photo with a value of the image path and name (used in Task 2) as a string
@@ -24,21 +51,44 @@
 /* OUTPUT */
 
 // Step 1: Assign the value of the name property (of the object declared above) to the HTML <span> element with an ID of name
+document.querySelector("#name").textContent = personalInfo.name
 
 // Step 2: Assign the value of the photo property as the src attribute of the HTML <img> element with an ID of photo
+document.querySelector("#photo").setAttribute("src", personalInfo.photo)
 
 // Step 3: Assign the value of the name property as the alt attribute of the HTML <img> element with an ID of photo
+document.querySelector("#photo").setAttribute("alt", personalInfo.name)
 
 // Step 4: For each favorite food in the favoriteFoods property, create an HTML <li> element and place its value in the <li> element
+personalInfo.favoriteFoods.forEach(food => {
+    let element = document.createElement("li")
+    element.textContent = food
+
+    document.querySelector("#favorite-foods").appendChild(element)
+})
 
 // Step 5: Append the <li> elements created above as children of the HTML <ul> element with an ID of favorite-foods
 
 // Step 6: Repeat Step 4 for each hobby in the hobbies property
+personalInfo.hobbies.forEach(hobby => {
+    let element = document.createElement("li")
+    element.textContent = hobby
+
+    document.querySelector("#hobbies").appendChild(element)
+})
 
 // Step 7: Repeat Step 5 using the HTML <ul> element with an ID of hobbies
 
 // Step 8: For each object in the <em>placesLived</em> property:
 // - Create an HTML <dt> element and put its place property in the <dt> element
 // - Create an HTML <dd> element and put its length property in the <dd> element
+personalInfo.placesLived.forEach((location) => {
+    let placeElement = document.createElement("dt")
+    let lengthElement = document.createElement("dd")
+    placeElement.textContent = location.place
+    lengthElement.textContent = location.length
 
+    document.querySelector('#places-lived').appendChild(placeElement);
+    document.querySelector('#places-lived').appendChild(lengthElement);
+})
 // Step 9: Append the HTML <dt> and <dd> elements created above to the HTML <dl> element with an ID of places-lived
